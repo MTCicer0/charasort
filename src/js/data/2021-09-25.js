a17c85a10aecb7b163500f2a91b3253edcb2baa3
MTCicer0 <91220262+MTCicer0@users.noreply.github.com>
2021-09-23 14:21:38 -0400

    Create 2021-09-25.js

---
+++ src/js/data/2021-09-25.js
@@ -0,0 +1,72 @@
+dataSetVersion = "2021-09-23"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  ];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Black-Gold-Navy",
+    img: "jsaDQk3.png",
+  },
+  {
+    name: "Black-Gold-Red",
+    img: "i2soLDJ.png",
+  },
+  {
+    name: "Black-White-Brown",
+    img: "MBSpRgL.png",
+  },
+  {
+    name: "Black-White-Navy",
+    img: "pJbPzbL.png",
+  },
+  {
+    name: "Black-White-Red",
+    img: "BGR82LJ.png",
+  },
+  {
+    name: "Brown-Gold-Brown",
+    img: "EFL1wrb.png",
+  },
+  {
+    name: "Brown-White-Grey",
+    img: "P9ounzc.png",
+  },
+  {
+    name: "Brown-White-Navy",
+    img: "SwFGR9V.png",
+  },
+  {
+    name: "Brown-White-Red",
+    img: "RLQW07d.png",
+  },
+  {
+    name: "Green-Gold-Red",
+    img: "1zYRigp.png",
+  },
+  {
+    name: "Green-White-Red",
+    img: "CGTkKOA.png",
+  },
+  {
+    name: "Tan-White-Brown",
+    img: "M7QuYHg.png",
+  },
+  {
+    name: "Tan-White-Navy",
+    img: "zAxidyV.png",
+  },
+  {
+    name: "Tan-White-Red",
+    img: "tZmEr1O.png",
+  },
+  {
+    name: "White-Gold-Navy",
+    img: "9dVowZs.png",
+  },
+  {
+    name: "White-Gold-Red",
+    img: "n112V4a.png",
+  }
+];
