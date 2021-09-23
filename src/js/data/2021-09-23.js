150584828d20640b93fc73a8892d462cc023d4a5
MTCicer0 <91220262+MTCicer0@users.noreply.github.com>
2021-09-23 11:47:31 -0400

    Create 2021-09-23.js

---
+++ src/js/data/2021-09-23.js
@@ -0,0 +1,276 @@
+dataSetVersion = "2021-09-23"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  ];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Black-Gold-Brown",
+    img: "45ShUKJ.png",
+  },
+  {
+    name: "Black-Gold-Grey",
+    img: "KVczPVH.png",
+  },
+  {
+    name: "Black-Gold-Navy",
+    img: "jsaDQk3.png",
+  },
+  {
+    name: "Black-Gold-Red",
+    img: "i2soLDJ.png",
+  },
+  {
+    name: "Black-Gold-White",
+    img: "DhzJ3CL.png",
+  },
+  {
+    name: "Black-Grey-Brown",
+    img: "W3c34G1.png",
+  },
+  {
+    name: "Black-Grey-Tan",
+    img: "TD6RyKe.png",
+  },
+  {
+    name: "Black-Grey-White",
+    img: "VXIBpre.png",
+  },
+  {
+    name: "Black-White-Brown",
+    img: "MBSpRgL.png",
+  },
+  {
+    name: "Black-White-Grey",
+    img: "Zb87y0k.png",
+  },
+  {
+    name: "Black-White-Navy",
+    img: "pJbPzbL.png",
+  },
+  {
+    name: "Black-White-Red",
+    img: "BGR82LJ.png",
+  },
+  {
+    name: "Brown-Black-Grey",
+    img: "qscAdKP.png",
+  },
+  {
+    name: "Brown-Black-Tan",
+    img: "GttiitL.png",
+  },
+  {
+    name: "Brown-Black-White",
+    img: "28j8jqX.png",
+  },
+  {
+    name: "Brown-Gold-Brown",
+    img: "EFL1wrb.png",
+  },
+  {
+    name: "Brown-Gold-Grey",
+    img: "9zO60bS.png",
+  },
+  {
+    name: "Brown-Gold-Navy",
+    img: "lDSbl9F.png",
+  },
+  {
+    name: "Brown-Gold-Red",
+    img: "JZz9EmI.png",
+  },
+  {
+    name: "Brown-Gold-White",
+    img: "U3fwVq0.png",
+  },
+  {
+    name: "Brown-Grey-Brown",
+    img: "Z81jWyT.png",
+  },
+  {
+    name: "Brown-Grey-Navy",
+    img: "I7xvXGx.png",
+  },
+  {
+    name: "Brown-Grey-Red",
+    img: "10QzHPg.png",
+  },
+  {
+    name: "Brown-Grey-Tan",
+    img: "wX4XiaY.png",
+  },
+  {
+    name: "Brown-Red-Tan",
+    img: "nk6Vlye.png",
+  },
+  {
+    name: "Brown-Red-White",
+    img: "nXOX5K7.png",
+  },
+  {
+    name: "Brown-White-Brown",
+    img: "0NGMOon.png",
+  },
+  {
+    name: "Brown-White-Grey",
+    img: "P9ounzc.png",
+  },
+  {
+    name: "Brown-White-Navy",
+    img: "SwFGR9V.png",
+  },
+  {
+    name: "Brown-White-Red",
+    img: "RLQW07d.png",
+  },
+  {
+    name: "Green-Black-Grey",
+    img: "DvzMVDd.png",
+  },
+  {
+    name: "Green-Black-Tan",
+    img: "C1wcQUW.png",
+  },
+  {
+    name: "Green-Gold-Brown",
+    img: "PsA0kuq.png",
+  },
+  {
+    name: "Green-Gold-Red",
+    img: "1zYRigp.png",
+  },
+  {
+    name: "Green-Grey-Brown",
+    img: "6MWAaUD.png",
+  },
+  {
+    name: "Green-Grey-Tan",
+    img: "EDxzszB.png",
+  },
+  {
+    name: "Green-Red-Brown",
+    img: "y38ThCi.png",
+  },
+  {
+    name: "Green-Red-White",
+    img: "O23oeEm.png",
+  },
+  {
+    name: "Green-White-Brown",
+    img: "w2FgsT0.png",
+  },
+  {
+    name: "Green-White-Red",
+    img: "CGTkKOA.png",
+  },
+  {
+    name: "Grey-Black-Tan",
+    img: "5tMRhmr.png",
+  },
+  {
+    name: "Grey-Black-White",
+    img: "cN1b54y.png",
+  },
+  {
+    name: "Grey-Gold-Brown",
+    img: "mrxgVFJ.png",
+  },
+  {
+    name: "Grey-Gold-Navy",
+    img: "gnr3dI6.png",
+  },
+  {
+    name: "Grey-Gold-Red",
+    img: "372qlw0.png",
+  },
+  {
+    name: "Grey-Gold-White",
+    img: "LODsZXR.png",
+  },
+  {
+    name: "Grey-White-Navy",
+    img: "KNaG1v4.png",
+  },
+  {
+    name: "Grey-White-Red",
+    img: "OB2iR5g.png",
+  },
+  {
+    name: "Tan-Black-Grey",
+    img: "k1OZVPo.png",
+  },
+  {
+    name: "Tan-Black-Tan",
+    img: "0Xn0HPr.png",
+  },
+  {
+    name: "Tan-Black-White",
+    img: "PvqL6uV.png",
+  },
+  {
+    name: "Tan-Gold-Brown",
+    img: "ZSGSk4h.png",
+  },
+  {
+    name: "Tan-Gold-Navy",
+    img: "lwtmJye.png",
+  },
+  {
+    name: "Tan-Gold-Red",
+    img: "BurEQFj.png",
+  },
+  {
+    name: "Tan-Grey-Brown",
+    img: "WG24y9v.png",
+  },
+  {
+    name: "Tan-Grey-White",
+    img: "zJOydYJ.png",
+  },
+  {
+    name: "Tan-Red-White",
+    img: "axDGygd.png",
+  },
+  {
+    name: "Tan-White-Brown",
+    img: "M7QuYHg.png",
+  },
+  {
+    name: "Tan-White-Grey",
+    img: "M7QuYHg.png",
+  },
+  {
+    name: "Tan-White-Navy",
+    img: "zAxidyV.png",
+  },
+  {
+    name: "Tan-White-Red",
+    img: "tZmEr1O.png",
+  },
+  {
+    name: "White-Black-Grey",
+    img: "KXN7jYb.png",
+  },
+  {
+    name: "White-Black-Tan",
+    img: "9XjyNRY.png",
+  },
+  {
+    name: "White-Gold-Navy",
+    img: "9dVowZs.png",
+  },
+  {
+    name: "White-Gold-Red",
+    img: "n112V4a.png",
+  },
+  {
+    name: "White-Red-Brown",
+    img: "i09jkLp.png",
+  },
+  {
+    name: "White-Red-Tan",
+    img: "DLweYqz.png",
+  }
+];
